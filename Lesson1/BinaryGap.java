dedc21efafd6f6f48e8092346c7df58d32f1639a
Merter Sualp <merter.sualp@gmail.com>
2017-02-27 22:04:48 +0300

    <# Enter your message here. #>

--- Lesson1/BinaryGap.java
+++ Lesson1/BinaryGap.java
@@ -6,30 +6,30 @@
 
 // Codility - Easy
 class BinaryGap {
-    public int solution(int N) {
-        int maxGap = 0;
-        int begin = mostSignificantBitForPositive(N);
-        while (begin != 0) {
-            N -= (1 << begin);
-            if (N == 0)
-                return maxGap;
-            int end = mostSignificantBitForPositive(N);
-            int gap = begin-end-1;
-            if (gap > maxGap)
-                maxGap = gap;
-            begin = end;
-        }
+  public int solution(int N) {
+    int maxGap = 0;
+    int begin = mostSignificantBitForPositive(N);
+    while (begin != 0) {
+      N -= (1 << begin);
+      if (N == 0)
         return maxGap;
+      int end = mostSignificantBitForPositive(N);
+      int gap = begin-end-1;
+      if (gap > maxGap)
+        maxGap = gap;
+      begin = end;
     }
-    
-    private int mostSignificantBitForPositive(int N) {
-        if (N <= 0)
-            return -1;
-        int pos = -1;
-        while (N != 0) {
-            N >>= 1;
-            pos++;
-        }
-        return pos;
+    return maxGap;
+  }
+
+  private int mostSignificantBitForPositive(int N) {
+    if (N <= 0)
+      return -1;
+    int pos = -1;
+    while (N != 0) {
+      N >>= 1;
+      pos++;
     }
+    return pos;
+  }
 }
